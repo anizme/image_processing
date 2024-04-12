f9037f1b5fcce2d4d2de7ccbe48fe2e5220a880a
Anizme <hirengtlesone@gmail.com>
2024-04-13 01:59:10 +0700

    fix

--- HW2/ex2.py
+++ HW2/ex2.py
@@ -13,16 +13,36 @@ import cv2
 import numpy as np
 from PIL import Image
 
+def only_read_img(img_path):
+    """
+        Read grayscale image
+        Inputs:
+        img_path: str: image path
+        Returns:
+        img: cv2 image
+    """
+    return cv2.imread(img_path, 0)
+
+def display_image(image, title="Image"):
+    """
+    Display an image using matplotlib. Rembember to use plt.show() to display the image
+    """
+    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    
+    plt.imshow(image_rgb)
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
+
 def read_img(img_path, img_size=(512, 512)):
-  """
-    + Đọc ảnh
-    + Chuyển thành grayscale
-    + Thay đổi kích thước ảnh thành img_size
-  """
-  img = cv2.imread(img_path, 0)
-  grayscale_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-  grayscale_img = cv2.resize(grayscale_img, img_size)
-  return grayscale_img
+    """
+      + Đọc ảnh
+      + Chuyển thành grayscale
+      + Thay đổi kích thước ảnh thành img_size
+    """
+    img = cv2.imread(img_path, 0)
+    img = cv2.resize(img, img_size)
+    return img
 
 
 def create_hybrid_img(img1, img2, r):
@@ -42,7 +62,7 @@ def create_hybrid_img(img1, img2, r):
   img2_ffshift = np.fft.fftshift(img2_fft)
 
   #create mask
-  x_grid, y_grid = np.meshgrid(np.arrange(h), np.arrange(w))
+  x_grid, y_grid = np.meshgrid(np.arange(h), np.arange(w))
   distance = np.sqrt((w // 2 - x_grid) ** 2 + (h // 2 - y_grid) ** 2)
   mask = distance <= r
 
@@ -54,15 +74,15 @@ def create_hybrid_img(img1, img2, r):
   f_img2_shift = np.fft.ifftshift(f_img2)
 
   img1 = np.fft.ifft2(f_img1_shift)
-  img2 = np.fft.iff2(f_img2_shift)
+  img2 = np.fft.ifft2(f_img2_shift)
 
   img = img1 + img2
-
+  img = np.abs(img)
   return img
 
 if __name__ == '__main__':
-  image_1_path = "ex2_images/karina.png"
-  image_2_path = "ex2_images/puppy.png"
+  image_1_path = "hw2/ex2_images/karina.png"
+  image_2_path = "hw2/ex2_images/puppy.png"
   img_1 = read_img(image_1_path)
   img_2 = read_img(image_2_path)
   hybrid_img = create_hybrid_img(img_2, img_1, 14)
@@ -73,3 +93,4 @@ if __name__ == '__main__':
   axes[1].axis("off")
   axes[2].imshow(hybrid_img, cmap="gray")
   axes[2].axis("off")
+  plt.show()
